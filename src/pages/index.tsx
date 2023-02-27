89b92a18c3ee490dbe0345b96c221d64b9461a01
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-02-27 09:55:21 -0300

    feat: created seo meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import { useKeenSlider } from "keen-slider/react";
 import Stripe from "stripe";
@@ -27,22 +28,28 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map(product => (
-        <Product 
-          key={product.id} 
-          href={`/product/${product.id}`} 
-          className="keen-slider__slide"
-          prefetch={false}
-        >
-          <Image src={product.imageUrl} width={520} height={480} alt=""/>
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
-      ))}
-    </HomeContainer>
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map(product => (
+          <Product 
+            key={product.id} 
+            href={`/product/${product.id}`} 
+            className="keen-slider__slide"
+            prefetch={false}
+          >
+            <Image src={product.imageUrl} width={520} height={480} alt=""/>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        ))}
+      </HomeContainer>
+    </>
   )
 }
 
